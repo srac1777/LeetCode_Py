2481dfafd21395728dc609061a9fc8810a6a1b51
srac1777 <shashank.racherla@gmail.com>
2021-08-10 09:33:34 -0400

    LC1-optimized

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -1,9 +1,17 @@
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum_n2(self, nums, target): #n^2 complexity
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+    def twoSum(self, nums, target): # O(n)
+        hash = {}
+        for i in range(0, len(nums)):
+            diff = target - nums[i]
+            if diff in hash:
+                return [i, hash[diff][1]]
+            hash[nums[i]] = (target - nums[i], i)
 
 sol = Solution().twoSum([3,3,5],8)
 print(sol)
