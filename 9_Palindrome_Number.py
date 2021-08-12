03d81dc681a0167e5ed7bb128302966d0ab6eb67
srac1777 <shashank.racherla@gmail.com>
2021-08-12 08:02:40 -0400

    LC9-space-optimized

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -4,11 +4,24 @@ class Solution:
         if s == s[::-1]:
             return True
         return False
+    
+    def num_reverse(self, x):
+        res = 0
+        while x > 0:
+            res *= 10
+            res += x % 10
+            x = int(x/10)
+        return res
+        
+    def getIsPalindromeUsingNumbers(self, x):
+        return x == self.num_reverse(x)
 
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, x):
         if x < 0:
             return False
         elif x < 10:
             return True
         else:
-            return self.getIsPalindrome(x)
+            return self.getIsPalindromeUsingNumbers(x)
+        
+    
