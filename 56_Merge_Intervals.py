f8deb404e42e1cdc40b84f4ece2ca3adef51b356
srac1777 <shashank.racherla@gmail.com>
2021-10-12 20:46:45 -0400

    56

---
+++ 56_Merge_Intervals.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortIntervals(self, intervals):
+        def sortFn(a):
+            return a[0]
+        return sorted(intervals, key=sortFn)
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) == 1:
+            return intervals
+        a = self.sortIntervals(intervals)
+        res = []
+
+        for interval in a:
+            if len(res) == 0:
+                res.append(interval)
+            elif interval[0] > res[-1][1]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+
+        return res
